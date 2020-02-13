9c83d9bd4f529945a398777340f8f6f0e94e2a95
bruno <bruno.bertossi@gmail.com>
2020-02-13 11:18:21 -0300

    Authenticate y tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_13_133952) do
+ActiveRecord::Schema.define(version: 2020_02_13_140223) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.string "pic"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Task.destroy_all
+Task.create(name: 'Comer Empanadas', pic: 'https://mouse.latercera.com/wp-content/uploads/2017/09/empanadas.jpg')
+Task.create(name: 'Tomar Terremoto', pic: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Terremotopiojera.jpg/1200px-Terremotopiojera.jpg')
+Task.create(name: 'Encumbrar Volantin', pic: 'https://www.sernac.cl/portal/619/articles-4110_imagen_01.thumb_i_normal.jpg')
+Task.create(name: 'Bailar Cueca', pic: 'https://www.grupoeducar.cl/wp-content//uploads/2018/09/cueca-1373x700.jpg')
+Task.create(name: 'Jugar Pichanga', pic: 'https://res.cloudinary.com/postedin/image/upload/d_sudandola:no-image.jpg,w_340,c_thumb,f_auto,q_80/sudandola/consejos-para-jugar-una-pichanga-bajo-altas-temperaturas.jpg')
+Task.create(name: 'Ir a una Fonda/Ramada', pic: 'https://www.carolina.cl/static/2016/09/sin-ttulo-7-2-1200x400.jpg')
+Task.create(name: 'Ir a la piojera', pic: 'https://m.cooperativa.cl/noticias/site/artic/20141216/imag/foto_0000000520141216203725.jpg')
+Task.create(name: 'Hacer un asado', pic: 'https://media.metrolatam.com/2018/02/13/asado660x495-1200x800.jpg')
+Task.create(name: 'Ir al Parque Ohiggins', pic: 'https://www.suksa.cl/web/wp-content/uploads/2017/02/parque-ohiggins.jpg')
